2abe1d707036d34c2717f02ce2fa3a77b49a8c51
Sie7he <diegoescurraromero@gmail.com>
2024-08-07 15:15:28 -0400

    recharts

--- cliente/src/pages/Dashboard.jsx
+++ cliente/src/pages/Dashboard.jsx
@@ -1,67 +1,60 @@
 import Card from "../components/shared/Card";
-import { Doughnut, Line } from "react-chartjs-2";
 import useDashboard from "../hooks/useDashboard";
 import Loading from "../components/shared/Loading";
-import Chart from "../components/shared/Chart";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, Cell, BarChart, Bar, Rectangle } from 'recharts';
 
 
 
 const Dashboard = () => {
   const { data, isLoading } = useDashboard();
   console.log(data)
-  const labels = data ? data.soldProducts.map((sp) => sp.nombre_producto) : [];
 
-  const options = {
-    responsive: true,
-    aspectRatio: 16 / 9,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-    },
+  const productosVendidos = data?.soldProducts.map(item => ({
+    ...item,
+    cantidad: Number(item.cantidad)
+  }));
+
+  const categoriasVendidas = data?.maxCategory.map(item => ({
+    ...item,
+    cantidad: Number(item.cantidad)
+  }));
+
+  const totalClientes = data?.totalClients.map(item => ({
+    ...item,
+    total: Number(item.total)
+  }))
+
+
+  const renderLegend = (props) => {
+    const { payload } = props;
+    return (
+      <div className="flex gap-4 justify-center">
+        {
+          payload.map((entry, index) => (
+            <span key={`item-${index}`} style={{ color: entry.color }}>
+              <span style={{ color: entry.color, marginRight: 8 }}>■</span>
+              {entry.payload.categoria}
+            </span>
+          ))
+        }
+      </div>
+    );
   };
 
-  const dataGraph = {
-    labels,
-    datasets: [
-      {
-        label: "Productos Vendidos",
-        data: data && data.soldProducts.map((sp) => sp.cantidad),
-        borderColor: "rgb(100, 210, 100)",
-        backgroundColor: "rgba(98, 99, 255, 0.7)",
-      },
-    ],
-  };
-  const labelsD = data ? data.maxCategory.map((cg) => cg.categoria) : [];
+  const COLORS = ['#8884d8', '#00C49F', '#FFBB28'];
 
-  const dataDonut = {
-    labels: labelsD,
-    datasets: [{
-      label: 'Cantidad Vendida',
-      data: data && data.maxCategory.map((cg) => cg.cantidad),
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-      ],
-      hoverOffset: 4
-    }]
-  };
-  const fechaActual = new Date();
-  const nombre_mes = fechaActual.toLocaleString("es", { month: "long" });
 
   if (isLoading) return <Loading />;
   return (
     <>
-      {data ? (
+    {data ? (
         <>
           <div className="flex flex-col p-6">
             <h3 className="text-2xl font-bold mb-4 capitalize shadow-sm border-b py-4 font-roboto">
-              Dashboard de {nombre_mes}
+              Dashboard de Agosto
             </h3>
           </div>
-          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 max-w-screen-xl mx-auto p-4 mb-10">
+          <section className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 max-w-screen-xl mx-auto p-4 mb-10">
             <Card
               title={"Objetivo"}
               content={"$30.000.000"}
@@ -82,15 +75,78 @@ const Dashboard = () => {
               )}
             />
 
-          </div>
-         <div className="grid lg:flex gap-4">
-         <Chart>
-            <Line  options={options} data={dataGraph} />
-          </Chart>
-          <Chart>
-          <Doughnut options={options} data={dataDonut}/>
-          </Chart>
-         </div>
+          </section>
+         
+
+       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-[56rem] lg:h-96 gap-2">
+      <article className="flex justify-center items-center shadow-md p-4">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={productosVendidos}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="nombre_producto" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="cantidad" stroke="#8884d8" activeDot={{ r: 8 }} />
+          
+        </LineChart>
+      </ResponsiveContainer>
+      </article>
+      <article className="flex justify-center items-center shadow-lg p-4">
+   <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={categoriasVendidas}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="cantidad"
+          >
+            {categoriasVendidas.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Legend content={renderLegend} />
+        </PieChart>
+      </ResponsiveContainer>
+      </article>
+      <article className="flex justify-center items-center shadow-lg p-4">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={totalClientes}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="nombre" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" fill="#00aae4" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+        </BarChart>
+      </ResponsiveContainer>
+      </article>
+       </section>
+
         </>
       ) : (
         <>
--- servidor/src/routes/analytics.js
+++ servidor/src/routes/analytics.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import pool from '../db/config.js'
+import express from "express";
+import pool from "../db/config.js";
 const router = express.Router();
 
-router.get('/dashboard', async (req, res) => {
-    try {
-     
+router.get("/dashboard", async (req, res) => {
+  try {
+    const sqlSumMonth =
+      "SELECT COALESCE(SUM(total), 0) AS TOTAL FROM venta WHERE EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM NOW()) ";
+    const sumTotal = await pool.query(sqlSumMonth);
 
-        const sqlSumMonth = 'SELECT COALESCE(SUM(total), 0) AS TOTAL FROM venta WHERE EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM NOW()) '
-        const sumTotal = await pool.query(sqlSumMonth)
-
-        const sqlMaxCategory = `SELECT c.categoria, SUM(v.cantidad) as cantidad
+    const sqlMaxCategory = `SELECT c.categoria, SUM(v.cantidad) as cantidad
         FROM categoria c
         JOIN producto p
         ON c.id_categoria = p.id_categoria
@@ -19,29 +18,34 @@ router.get('/dashboard', async (req, res) => {
         ON venta.id_venta = v.id_venta
 		WHERE EXTRACT (MONTH FROM fecha) = EXTRACT(MONTH FROM NOW())
         GROUP BY categoria
-        ORDER BY cantidad DESC`
-        const maxCategory = await pool.query(sqlMaxCategory);
+        ORDER BY cantidad DESC`;
+    const maxCategory = await pool.query(sqlMaxCategory);
 
-
-        const sqlProducts = `SELECT p.nombre_producto, SUM(v.cantidad ) AS cantidad 
+    const sqlProducts = `SELECT p.nombre_producto, SUM(v.cantidad ) AS cantidad 
         FROM producto p 
         JOIN venta_detalle v 
         ON p.id_producto = v.id_producto 
         JOIN venta ON v.id_venta = venta.id_venta
         WHERE EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM NOW())
-        GROUP BY nombre_producto ORDER BY cantidad`
-        const soldProducts = await pool.query(sqlProducts)
+        GROUP BY nombre_producto ORDER BY cantidad`;
+    const soldProducts = await pool.query(sqlProducts);
 
-        return res.status(200).json({
-            sumTotal : sumTotal.rows[0],
-            maxCategory : maxCategory.rows,
-            soldProducts: soldProducts.rows
+    const sqlTotalClients = `SELECT cli.nombre, SUM(v.total) as total
+        FROM cliente cli
+        JOIN venta v
+        ON cli.id_cliente = v.id_cliente
+        GROUP BY nombre`;
+    const totalClients = await pool.query(sqlTotalClients);
 
-        })
-    } catch (error) {
-        return res.status(500).send('Lo sentimos, ha ocurrido un error')
-    }
-})
+    return res.status(200).json({
+      sumTotal: sumTotal.rows[0],
+      maxCategory: maxCategory.rows,
+      soldProducts: soldProducts.rows,
+      totalClients: totalClients.rows
+    });
+  } catch (error) {
+    return res.status(500).send("Lo sentimos, ha ocurrido un error");
+  }
+});
 
-
-export default router
+export default router;
